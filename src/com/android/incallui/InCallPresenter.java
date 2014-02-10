80a31e509584cd4a39c8a905e8b1ecf466dafb92
Jake Whatley <jake@nameless-rom.org>
2014-02-10 14:06:43 -0500

    InCallUI: enable navbar while call is incoming
    since we have back-button support to send incoming calls to
    the statusbar, we need to make sure the navbar is actually enabled
    while a call is incoming, otherwise that feature is totally unavailable
    on devices that do not have hardware back button
    
    Change-Id: I2eb40b127389feb896c8c762f3ce0609f8b8a4db

--- src/com/android/incallui/InCallPresenter.java
+++ src/com/android/incallui/InCallPresenter.java
@@ -280,10 +280,10 @@ public class InCallPresenter implements CallList.Listener {
         Log.i(this, "Phone switching state: " + mInCallState + " -> " + newState);
         mInCallState = newState;
 
-        // Disable notification shade and soft navigation buttons
+        // Enable the soft navigation buttons
         // on new incoming call as long it is no background call
         if (newState.isIncoming() && !mCallUiInBackground) {
-            CallCommandClient.getInstance().setSystemBarNavigationEnabled(false);
+            CallCommandClient.getInstance().setSystemBarNavigationEnabled(true);
             if (mAccelerometerListener != null) {
                 mAccelerometerListener.enableSensor(true);
             }
